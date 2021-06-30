8fdb91e6bd60a6cba4531b19fb93e9a65a80f585
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-07-01 07:13:20 +0900

    11947번: 과목선택

---
+++ 11000/11948.py
@@ -0,0 +1,22 @@
+"""
+JOI는 물리, 화학, 생물, 지구과학, 역사, 지리 총 6 과목의 시험을 봤다. 각 시험의 만점은 100점이다.
+
+JOI는 물리, 화학, 생물, 지구과학 4과목 중에서 3 과목을 선택하고 역사, 지리 2 과목 중에서 한 과목을 선택한다.
+
+시험 점수의 합이 가장 높게 나오도록 과목을 선택할 때, JOI가 선택한 과목의 시험 점수의 합을 구하시오.
+
+"""
+
+test1 = []
+test2 = []
+
+for i in range(4):
+    test1.append(int(input()))
+for i in range(2):
+    test2.append(int(input()))
+
+test1.sort(reverse=True)
+test2.sort(reverse=True)
+
+total = sum(test1[:3]) + test2[0]
+print(total)
