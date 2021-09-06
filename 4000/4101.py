4b2be1acd2d289302cb4d0bd2512c94566b4db97
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-09-07 07:46:22 +0900

    4101번: 크냐?

--- 4000/4101.py
+++ 4000/4101.py
@@ -2,8 +2,4 @@ while True:
     a, b = map(int, input().split())
     if a == 0 and b == 0:
         break
-    if a > b:
-        print("Yes")
-    else:
-        print("No")
-
+    print('Yes' if a > b else 'No')
